f6715fe25a5ec686c3566014adeb027a7c130bee
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2015-11-20 06:44:15 -0800

    homework solution for day 4

--- change/base.js
+++ change/base.js
@@ -3,5 +3,8 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
   $("#time").text( Date.now() );
-
-})
+  $("input.add").on("change", function handleChange(event) {
+    var left = $("#left").val();
+    var right = $("#right").val();
+    });
+});
